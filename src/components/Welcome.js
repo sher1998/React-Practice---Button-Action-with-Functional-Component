f68c1df4c0d50dcf5415cc6601abc926e3828612
Sher Pal Singh <88039223+sher1998@users.noreply.github.com>
2021-08-20 21:13:51 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react';
 
+// function display(){
+//   return React.createElement('p',{id:"para"},"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.");;
+// }
+function Welcome() {
+  
+  let [x,display] = useState(null);
+  // console.log(x);
+  return <div>
+    <button onClick= {() => display(x = <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)} id="click">click</button> ;
+    {/* <p id="para">{x}</p> */ x}
+    </div>
+}
+
+export default Welcome;
