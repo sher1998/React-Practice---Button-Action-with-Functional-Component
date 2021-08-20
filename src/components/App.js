9f29ed9e8b2f82f835a78d8160fdb4b5b07efdd1
Sher Pal Singh <88039223+sher1998@users.noreply.github.com>
2021-08-20 21:11:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import Welcome from "./Welcome";
 
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+    
+    <Welcome/>
     </div>
   );
 }
